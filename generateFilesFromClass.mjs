c992e00348dbbf2e71805af20da18a135b37dfd1
Pathetic Geek <geekpathetic@gmail.com>
2022-07-29 18:50:52 +0530

    sadasdassadasd

--- generateFilesFromClass.mjs
+++ generateFilesFromClass.mjs
@@ -1,21 +1,31 @@
 #!/usr/bin/env zx
 const css = require('css')
 
-const file = fs.readFileSync("./generatedTailwind.css", "utf8")
+const file = fs.readFileSync('./generatedTailwind.css', 'utf8')
 
 const cssTree = css.parse(file)
 
-let classNames = cssTree.stylesheet.rules.filter((rule => rule.selectors?.[0].startsWith("."))) 
+let classNames = cssTree.stylesheet.rules.filter((rule) =>
+  rule.selectors?.[0].startsWith('.')
+)
 
-const mediaQueries = cssTree.stylesheet.rules.filter((rule => rule.type === "media"))
+const mediaQueries = cssTree.stylesheet.rules.filter(
+  (rule) => rule.type === 'media'
+)
 
-const extraClassNames = mediaQueries.map((query => query.rules.filter((rule => rule.selectors?.[0].startsWith("."))))).flat()
+const extraClassNames = mediaQueries
+  .map((query) =>
+    query.rules.filter((rule) => rule.selectors?.[0].startsWith('.'))
+  )
+  .flat()
 
 classNames = classNames.concat(extraClassNames)
 
 // console.log(classNames)
 
-const classXyz = classNames.map((className => className.selectors?.[0].slice(1))).join(" ")
+const classXyz = classNames
+  .map((className) => className.selectors?.[0].slice(1).replace(/\\/g, ''))
+  .join(' ')
 // console.log(classXyz)
 
 const div = `<div className="${classXyz}"></div>`
